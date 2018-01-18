0e4ab823ec4eeffc26c3f0221de114a886722510
Charles Reid <charlesreid1@gmail.com>
2018-01-17 20:42:54 -0800

    Rewrite install script to run apt-get on each individual package.
    Reason for doing this is simple: if "apt-get install A B C D" fails on A, B/C/D are not installed. This is not a problem when package names are predictable, but apt-get package naming is a clusterfuck.

--- install_packages.sh
+++ install_packages.sh
@@ -9,48 +9,35 @@ if [ "$(id -u)" != "0" ]; then
     exit 1;
 fi
 
-apt-get install -y \
-    aptitude \
-    apache \
-    build-essential \
-    git \
-    vim \
-    screen \
-    tmux \
-    tshark \
-    tcpdump \
-    stunnel \
-    wireshark \
-    net-tools
+apt-get install -y git 
+apt-get install -y vim
+apt-get install -y screen
+apt-get install -y aptitude
+apt-get install -y build-essential
+apt-get install -y tmux
+apt-get install -y tshark
+apt-get install -y tcpdump
+apt-get install -y stunnel
+apt-get install -y wireshark
+apt-get install -y net-too
+apt-get install -y python3
+apt-get install -y python-dev
+apt-get install -y python-q
+apt-get install -y golang
+apt-get install -y perl
+apt-get install -y octave
+apt-get install -y gnuplo
+apt-get install -y texlive-ful
+apt-get install -y libatlas-dev
+apt-get install -y libpng-dev
+apt-get install -y libfreetype6
+apt-get install -y libfreetype6-dev
+apt-get install -y libzmq3-dev
+apt-get install -y liblapack-d
+apt-get install -y gfortran
+apt-get install -y gcc
+apt-get install -y g++
+apt-get install -y libc++-dev
+apt-get install -y libstdc++-6-dev
 
-apt-get install -y \
-    python3 \
-    python-dev \
-    python-qt4
-
-apt-get install -y \
-    golang \
-    perl \
-    octave \
-    gnuplot 
-
-apt-get install -y \
-    texlive-full 
-
-apt-get install -y \
-    libatlas-dev \
-    libpng-dev \
-    libfreetype6 \
-    libfreetype6-dev \
-    libzmq3-dev \
-    liblapack-dev \
-    build-essential 
-
-apt-get install -y \
-    gfortran \
-    gcc \
-    g++ \
-    libc++-dev \
-    libstdc++-6-dev
-
-apt autoremove
+apt -y autoremove
