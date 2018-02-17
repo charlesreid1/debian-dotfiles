58cffaf81d36426637400e43aba99bc4ad836b8b
Charles Reid <charlesreid1@gmail.com>
2018-02-16 23:39:49 -0800

    fix jupiter install packages - root only

--- jupiter_install_packages.sh
+++ jupiter_install_packages.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+if [ "$(id -u)" != "0" ]; then
+    echo ""
+    echo ""
+    echo "This script should be run as root."
+    echo ""
+    echo ""
+    exit 1;
+fi
+
 # wireless
 apt-get install -y wireless-tools
 apt-get install -y aircrack-ng
 apt-get install -y net-tools
 apt-get install -y reaver
 apt-get install -y john
-apt-get install -y lua5.3
-apt-get install -y liblua5.3-0
-apt-get install -y liblua5.3-dev
 
 ## Ideally, would add wifite to a 
 ## directory that is on $PATH
