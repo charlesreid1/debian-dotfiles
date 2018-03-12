06ce1db3da4fa50aee80c7b8b43755f30b2d6f26
Charles Reid <charlesreid1@gmail.com>
2018-03-12 02:29:18 -0700

    trying to fix make user script

--- make_user_charles.sh
+++ make_user_charles.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-adduser --quiet charles
-echo "charles:zeno135" | chpasswd
-usermod -aG sudo charles
+sudo su -c "useradd charles -s /bin/bash -m -g charles -G sudo"
+sudo chpasswd << 'END'
+charles:zeno135
+END
 
-mkdir /home/charles/.ssh
-chown charles:charles /home/charles/.ssh
-chmod 700 /home/charles/.ssh
-touch /home/charles/.ssh/authorized_keys
-chmod 600 /home/charles/.ssh/authorized_keys
+#mkdir /home/charles/.ssh
+#chown charles:charles /home/charles/.ssh
+#chmod 700 /home/charles/.ssh
+#touch /home/charles/.ssh/authorized_keys
+#chmod 600 /home/charles/.ssh/authorized_keys
 
