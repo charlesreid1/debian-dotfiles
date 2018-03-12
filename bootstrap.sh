a8f2280cf1fce70ead3715479442aec8ddd70ec8
Charles Reid <charlesreid1@gmail.com>
2018-03-12 05:02:11 -0700

    move bootstrap to correct location.

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,22 +10,10 @@ git pull origin master;
 function doIt() {
 	rsync \
 		--exclude ".git/" \
-		--exclude "add_charles_to_docker.sh" \
 		--exclude "bootstrap.sh" \
-		--exclude "fix_ssh.sh" \
-		--exclude "gen_ssh_keys.sh" \
-		--exclude "get-docker.sh" \
 		--exclude "greenfield_codes.sh" \
-		--exclude "install_packages.sh" \
-		--exclude "install_pyenv.sh" \
-		--exclude "jupiter-apt-stash.txt" \
 		--exclude "jupiter_install_packages.sh" \
-		--exclude "make_user_charles.sh" \
-		--exclude "pre_bootstrap.sh" \
-		--exclude "python_install.sh" \
-		--exclude "python_setup.sh" \
-		--exclude "remove_packages.sh" \
-		--exclude "set_machine_name.sh" \
+		--exclude "jupiter_apt-stash.txt" \
 		--exclude "README.md" \
 		--exclude "LICENSE" \
 		-avh --no-perms . ~;
--- tasks_charles/charles_init.sh
+++ tasks_charles/charles_init.sh
@@ -2,6 +2,7 @@
 #
 # initialize all the dotfiles stuff
 # for user charles
+set -x
 
 DOTFILES="$HOME/codes/dotfiles"
 mkdir -p $DOTFILES
@@ -12,7 +13,10 @@ git clone https://charlesreid1.com:3000/dotfiles/debian.git $DEBIAN
 cd $DEBIAN/tasks_charles
 
 ./pre_bootstrap.sh
+(
+cd ../
 ./bootstrap.sh -f
+)
 ./install_pyenv.sh
 ./python_install.sh
 ./python_setup.sh
