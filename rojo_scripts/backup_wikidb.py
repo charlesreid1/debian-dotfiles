09842922d56467b39254e1a739619231d5b622d7
Charles Reid <charlesreid1@gmail.com>
2018-03-24 00:29:01 -0700

    creating or updating wiki backups.

--- rojo_scripts/backup_wikidb.py
+++ rojo_scripts/backup_wikidb.py
@@ -3,32 +3,20 @@ from datetime import datetime
 import subprocess
 import socket
 
-
 """
-Back Up Charlesreid1.com Wiki
-
-Size: Medium
-
-This script does the following:
-- back up wiki dump
-- zip wiki files (images, config files, skins, etc.)
+Back Up Charlesreid1.com Wiki Database 
 """
 
-
-def backup_wiki(basedir):
-
-    # create backup dir
+def backup_wikidb(basedir):
+# create backup dir
     subprocess.call(["mkdir","-p",basedir])
-    
-    #####################
-    # Start specific task
-    
+
     # load mysql username/password
     with open('rojo.mysql.password','r') as f:
         credentials = f.readlines()
     u = credentials[0].strip()
     p = credentials[1].strip()
-    
+
     # back up wiki db
     userarg = "--user=%s"%(u)
     pwarg = "--password=%s"%(p)
@@ -43,26 +31,15 @@ def backup_wiki(basedir):
     with open(wikidump,'wb') as f:
         subprocess.call(wikicmd, cwd=basedir, stdout=f)
 
-    # compress wiki files
-    wikitar = basedir+"/wiki_files.tar.gz"
-    wikidir = "/www/w"
-    subprocess.call(["tar","czf",wikitar,wikidir])
-    
-    # End specific task
-    #####################
-    
-    print("Done backing up wiki to %s"%basedir)
-
 
 if __name__=="__main__":
 
     host = socket.gethostname()
 
-    dat = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    dat = datetime.now().strftime("%Y-%m-%d_%H-%M")
 
     if(host=="rojo"):
         basedir = "/junkinthetrunk/backups/wiki_"+dat
-        backup_wiki(basedir)
+        backup_wikidb(basedir)
     else:
         raise Exception("You aren't rojo - you probably didn't mean to run this script!")
-
