87e59cc64a904da20c8410c380cea92f99305235
Charles Reid <charlesreid1@gmail.com>
2018-03-24 15:31:32 -0700

    add docker compose to docker install script

--- tasks_sudo/get_docker.sh
+++ tasks_sudo/get_docker.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
 bash <( curl https://get.docker.com/ )
+docker --version
+
+sudo curl -L https://github.com/docker/compose/releases/download/1.20.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
 
