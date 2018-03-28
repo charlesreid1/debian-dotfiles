aabd55c14532c5926996000dcc7e16e51b4d2df2
Charles Reid <charlesreid1@gmail.com>
2018-03-27 19:57:05 -0700

    add disable ipv6 script

--- tasks_sudo/disable_ipv6.sh
+++ tasks_sudo/disable_ipv6.sh
@@ -9,4 +9,7 @@ if [ "$(id -u)" != "0" ]; then
     exit 1;
 fi
 
-usermod -aG docker charles
+echo "net.ipv6.conf.all.disable_ipv6 = 1
+net.ipv6.conf.default.disable_ipv6 = 1
+net.ipv6.conf.lo.disable_ipv6 = 1" >> /etc/sysctl.conf
+
