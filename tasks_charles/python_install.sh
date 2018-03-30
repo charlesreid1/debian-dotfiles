b175161d646c5319129b8ac5dd90cb9d3c2cb981
Charles Reid <charlesreid1@gmail.com>
2018-03-30 13:10:07 -0700

    update python install to include pelican

--- tasks_charles/python_install.sh
+++ tasks_charles/python_install.sh
@@ -14,20 +14,11 @@ if [ "$(id -u)" == "0" ]; then
     exit 1;
 fi
 
-pip2 install -U --user numpy scipy pandas
 pip3 install -U --user numpy scipy pandas
-
-pip2 install -U --user matplotlib seaborn
 pip3 install -U --user matplotlib seaborn
-
-pip2 install -U --user jupyter ipython
 pip3 install -U --user jupyter ipython
-
-pip2 install -U --user tornado pyzmq pygments
 pip3 install -U --user tornado pyzmq pygments
-
-pip2 install -U --user pygments pillow pelican
 pip3 install -U --user pygments pillow pelican
-
-pip2 install -U --user virtualenv conda
 pip3 install -U --user virtualenv conda
+pip3 install -U --user pelican
+
