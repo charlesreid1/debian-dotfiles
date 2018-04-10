0922d1c3f622180734cc74b6fc023db265df63f4
Charles Reid <charlesreid1@gmail.com>
2018-04-09 20:22:18 -0700

    commit clone scripts

---
+++ tasks/clone/charles_clone_charlesreid1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+CHAZ="$HOME/codes/charlesreid1"
+mkdir -p $CHAZ
+cd $CHAZ
+
+git clone --recursive https://git.charlesreid1.com/charlesreid1/bot-master.git bot-master
+git clone --recursive https://git.charlesreid1.com/charlesreid1/charlesreid1-master.git charlesreid1-master
+
---
+++ tasks/clone/charles_clone_hacking.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+SEC="$HOME/codes/security"
+mkdir -p $SEC
+cd $SEC
+
+git clone https://github.com/danielmiessler/SecLists.git
+
