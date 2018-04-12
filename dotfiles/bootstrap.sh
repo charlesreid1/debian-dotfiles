37979fe29832eb4d98e25db852a1084205f7fab2
Charles Reid <charlesreid1@gmail.com>
2018-04-12 00:03:51 -0700

    allow for machine-specific scripts directory

--- dotfiles/bootstrap.sh
+++ dotfiles/bootstrap.sh
@@ -11,21 +11,28 @@
 
 git pull origin master;
 
+if [[ "$HOSTNAME" == "jupiter" ]]; then
+    EXTRA_EXCLUDE=''
+else
+    EXTRA_EXCLUDE='--exclude "jupiter_scripts"'
+fi
+
 function doIt() {
-	rsync \
-		--exclude ".git/" \
-		--exclude "bootstrap.sh" \
-		-avh --no-perms . ~;
-	source ~/.bash_profile;
+    rsync \
+        --exclude ".git/" \
+        --exclude "bootstrap.sh" \
+        ${EXTRA_EXCLUDE} \
+        -avh --no-perms . ~;
+    source ~/.bash_profile;
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
+    doIt;
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
-	fi;
+    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+    echo "";
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        doIt;
+    fi;
 fi;
 unset doIt;
