8adee3c9aeb7c41b81457f3fd16937bd827536bc
Charles Reid <charlesreid1@gmail.com>
2018-04-12 02:33:37 -0700

    add copy authorized keys from ubuntu to charles

--- tasks/secrets/sudo_secrets_all.sh
+++ tasks/secrets/sudo_secrets_all.sh
@@ -6,3 +6,20 @@ $secrets_dir/sudo_fix_ssh.sh
 $secrets_dir/sudo_gen_ssh_keys.sh
 $secrets_dir/sudo_install_certbot.sh
 
+$secrets_dir/charles_gen_ssh_keys.sh
+
+if [[ "$HOSTNAME" == "blackbeard" ]]; then
+    # aws only:
+    # copy authorized key
+    # from ubuntu to charles
+    cp /home/ubuntu/.ssh/authorized_keys /home/charles/.ssh/authorized_keys
+    chown charles:charles /home/charles/.ssh/authorized_keys
+fi
+
+# If we are on another host, 
+# we need to set the password manually.
+# boooooo.
+# 
+# alternatively, grab a public key
+# using wget or something.
+
