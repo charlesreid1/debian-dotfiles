6956c96557b74c104078736480f757486f96c2f2
Charles Reid <charlesreid1@gmail.com>
2018-04-11 23:11:30 -0700

    update clone scripts to check hostname

--- tasks/clone/charles_clone_docker.sh
+++ tasks/clone/charles_clone_docker.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-DOCKER="$HOME/codes/docker"
-mkdir -p $DOCKER
-cd $DOCKER
-
-git clone --recursive https://git.charlesreid1.com/docker/d-mysql.git
-git clone             https://git.charlesreid1.com/docker/d-phpmyadmin.git
-git clone --recursive https://git.charlesreid1.com/docker/d-mediawiki.git
-git clone             https://git.charlesreid1.com/docker/d-charlesreid1-utils.git
-git clone             https://git.charlesreid1.com/docker/pod-charlesreid1-wiki.git
+if [[ "$HOSTNAME" == "krash" ]]; then
+    DOCKER="$HOME/codes/docker"
+    mkdir -p $DOCKER
+    cd $DOCKER
+    
+    git clone --recursive https://git.charlesreid1.com/docker/pod-charlesreid1-wiki.git
+fi
 
