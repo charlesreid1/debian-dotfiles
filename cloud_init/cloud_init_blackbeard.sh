d9d9b582a40350dc1cb0f1a97049d575476e6a91
Charles Reid <charlesreid1@gmail.com>
2018-04-12 01:22:07 -0700

    update blackbeard init script - root is okay. now adding in user.

--- cloud_init/cloud_init_blackbeard.sh
+++ cloud_init/cloud_init_blackbeard.sh
@@ -14,3 +14,10 @@ git clone https://git.charlesreid1.com/dotfiles/debian $DOTFILES
 # run root init script
 $DOTFILES/tasks/sudo_all.sh blackbeard
 
+# copy the user init script
+cp $DOTFILES/tasks/charles_init.sh /home/$USER/.
+chown $USER:$USER /home/$USER/user_init.sh
+
+# run user init script as user
+sudo -H -i -u $USER /home/$USER/user_init.sh
+
