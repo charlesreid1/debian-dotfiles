51b6aaea5085456c7ed3809a1bf7ad46fb729a72
Charles Reid <charlesreid1@gmail.com>
2018-04-12 03:07:12 -0700

    update ssh keygen to be non-interactive

--- tasks/secrets/charles_gen_ssh_keys.sh
+++ tasks/secrets/charles_gen_ssh_keys.sh
@@ -4,7 +4,7 @@
 # pre-baked public keys.
 set -x
 
-ssh-keygen -f $HOME/.ssh/id_rsa -t rsa -N ''
+yes | ssh-keygen -f $HOME/.ssh/id_rsa -t rsa -N ''
 chmod 700 $HOME/.ssh
 touch $HOME/.ssh/authorized_keys
 chmod 600 $HOME/.ssh/authorized_keys
