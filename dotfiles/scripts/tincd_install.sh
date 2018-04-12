02176d4ffbe9abb64e512a216af0b0e1c7afaf90
Charles Reid <charlesreid1@gmail.com>
2018-04-12 00:03:04 -0700

    add tincd machine-specific install script

---
+++ dotfiles/scripts/tincd_install.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+#
+# Install tincd
+#
+# krash = 10.6.0.1
+# blackbeard = 10.6.0.244
+# jupiter = 10.6.0.36
+#
+# hard-coded to avoid string variable mixups.
+
+if [ "$(id -u)" != "0" ]; then
+    echo ""
+    echo ""
+    echo "This script should be run as root."
+    echo ""
+    echo ""
+    exit 1;
+fi
+
+LABEL="master"
+
+apt-get install -y tinc
+
+mkdir -p /etc/tinc/$LABEL
+
+# -----------
+
+if [[ "$HOSTNAME" == "krash" ]]; then
+
+    cat > /etc/tinc/$LABEL/tinc.conf <<EOL
+Name = krash
+AddressFamily = any
+Mode = switch
+EOL
+
+    cat > /etc/tinc/$LABEL/tinc-up <<EOL
+#!/bin/sh
+ifconfig $INTERFACE 10.6.0.36 netmask 255.255.0.0
+EOL
+
+# -----------
+
+elif [[ "$HOSTNAME" == "blackbeard" ]]; then
+
+    cat > /etc/tinc/$LABEL/tinc.conf <<EOL
+Name = blackbeard
+AddressFamily = any
+Mode = switch
+EOL
+
+    cat > /etc/tinc/$LABEL/tinc-up <<EOL
+#!/bin/sh
+ifconfig $INTERFACE 10.6.0.244 netmask 255.255.0.0
+EOL
+
+# -----------
+
+elif [[ "$HOSTNAME" == "jupiter" ]]; then
+
+    cat > /etc/tinc/$LABEL/tinc.conf <<EOL
+Name = blackbeard
+AddressFamily = any
+Mode = switch
+EOL
+
+    cat > /etc/tinc/$LABEL/tinc-up <<EOL
+#!/bin/sh
+ifconfig $INTERFACE 10.6.0.36 netmask 255.255.0.0
+EOL
+
+fi
+
+# -----------
+
+cat > /etc/tinc/$LABEL/tinc-down <<EOL
+#!/bin/sh
+ifconfig $INTERFACE down
+EOL
+
+git clone https://git.charlesreid1.com/charlesreid1/tinc-hosts.git /etc/tinc/$LABEL/hosts
+
