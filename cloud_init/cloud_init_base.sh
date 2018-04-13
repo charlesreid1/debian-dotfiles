be19e9f6058ef39d16b918c1a60b804c869799b1
Charles Reid <charlesreid1@gmail.com>
2018-04-12 20:26:03 -0700

    fix typo in script name in cloud init base

--- cloud_init/cloud_init_base.sh
+++ cloud_init/cloud_init_base.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
-#
-# This script is intended to get you from
-# bare metal to a machine ready for 
-# charlesreid1 tasks.
-#
-# blackbeard - bots/hooks
-# krash - charlesreid1 pod
-# jupiter - utility
-#
-# Run as a one-shot installer:
-# bash <( curl INSERT_SCRIPT_URL_HERE )
-# 
 set -x
 
 USER="charles"
@@ -19,22 +7,17 @@ USER="charles"
 apt-get update
 apt-get install -y git
 
-#######################
-# First, the root user 
-# runs the dotfiles
-# installation script
-
 # check out root user dotfiles at /root/dotfiles
 DOTFILES="$HOME/dotfiles"
 git clone https://git.charlesreid1.com/dotfiles/debian $DOTFILES
 
 # run root init script
-$DOTFILES/tasks_sudo/sudo_init.sh MACHINENAME
+$DOTFILES/tasks/sudo_all.sh blackbeard
 
 # copy the user init script
-cp $DOTFILES/tasks_user/user_init.sh /home/$USER/.
-chown $USER:$USER /home/$USER/user_init.sh
+cp $DOTFILES/tasks/charles_init.sh /home/$USER/.
+chown $USER:$USER /home/$USER/charles_init.sh
 
 # run user init script as user
-sudo -H -i -u $USER /home/$USER/user_init.sh
+sudo -H -i -u $USER /home/$USER/charles_init.sh
 
