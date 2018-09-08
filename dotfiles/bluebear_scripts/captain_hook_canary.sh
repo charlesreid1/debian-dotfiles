8e749d564c7ca51c786a33cb0b60a0b9d7f26b3a
Charles Reid <charlesreid1@gmail.com>
2018-09-08 15:49:01 -0700

    add bluebear scripts

---
+++ dotfiles/bluebear_scripts/captain_hook_canary.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+: '
+Captain Hook Canary Script
+
+
+Note: this needs an associated systemd service.
+See the services directory of the dotfiles repo.
+
+This is a canary script for connecting
+the Captain Hook container to the host 
+machine, and triggering tasks on the 
+host machine with webhooks.
+
+The Captain Hook container mounts the 
+following host directory inside the 
+container (same location for host/container):
+
+/tmp/triggers/
+
+When a webhook in Captain Hook wants to 
+trigger an event on the host (blackbeard),
+it puts a file in /tmp/triggers/.
+
+Meanwhile, on the host, this script checks
+every 10 seconds for trigger files.
+
+Each webhook can create its own trigger file,
+and this script processes each trigger differently.
+'
+
+while true
+do
+    # bootstrap-pull captain hook
+    if [ -f "/tmp/triggers/push-b-captain-hook-master" ]; then
+        echo "CAPTAIN HOOK'S CANARY:"
+        echo "Running trigger to update Captain Hook on the host machine (user charles)"
+        sudo -H -u charles python /home/charles/scripts/blackbeard_scripts/captain_hook_pull_host.py
+        echo "All done."
+        rm -f "/tmp/triggers/push-b-captain-hook-master"
+        touch /tmp/canary-yup-host-works
+    fi
+
+    sleep 10;
+done
+
---
+++ dotfiles/bluebear_scripts/captain_hook_pull_host.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+import subprocess
+import os
+import time
+
+"""
+Captain Hook: Pull Captain Hook on the Host 
+
+This script is called by the host machine 
+(blackbeard) running the Captain Hook container.
+
+This is triggered by push actions to the 
+master branch of b-captain-hook.
+
+The action is to update (git pull) the copy 
+of Captain Hook running on the host, and
+restart the container pod.
+"""
+
+work_dir = os.path.join('/home','charles','codes','docker','pod-webhooks','b-captain-hook')
+pod_dir = os.path.join('/home','charles','codes','docker','pod-webhooks')
+
+from datetime import datetime
+d = datetime.now().strftime('%Y-m-%d')
+with open('/tmp/captain_hook_pull_host_%s.log'%(d),'w') as f:
+
+    # Step 1:
+    # Update Captain Hook
+    co_cmd = ['git','checkout','master']
+    subprocess.call(co_cmd, cwd=work_dir, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    stdout, stderr = proc.communicate()
+    o = stdout.decode('utf-8')
+    e = stderr.decode('utf-8')
+    f.write(" ".join(co_cmd))
+    f.write("\n")
+    f.write("-"*40)
+    f.write("\n")
+    f.write(o)
+    f.write("\n")
+    f.write(e)
+    f.write("\n\n")
+    
+    f_cmd = ['git','fetch','--all']
+    subprocess.call(f_cmd, cwd=work_dir, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    
+    stdout, stderr = proc.communicate()
+    o = stdout.decode('utf-8')
+    e = stderr.decode('utf-8')
+    f.write(" ".join(f_cmd))
+    f.write("\n")
+    f.write("-"*40)
+    f.write("\n")
+    f.write(o)
+    f.write("\n")
+    f.write(e)
+    f.write("\n\n")
+    time.sleep(5)
+    
+    pull_cmd = ['git','pull','cmr','master']
+    subprocess.call(pull_cmd, cwd=work_dir, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    
+    stdout, stderr = proc.communicate()
+    o = stdout.decode('utf-8')
+    e = stderr.decode('utf-8')
+    f.write(" ".join(pull_cmd))
+    f.write("\n")
+    f.write("-"*40)
+    f.write("\n")
+    f.write(o)
+    f.write("\n")
+    f.write(e)
+    f.write("\n\n")
+    time.sleep(10)
+    
+    # Step 2:
+    # Restart Captain Hook pod
+    pod_restart = ['docker-compose','restart']
+    subprocess.call(pod_restart, cwd=pod_dir, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    stdout, stderr = proc.communicate()
+    o = stdout.decode('utf-8')
+    e = stderr.decode('utf-8')
+    f.write(" ".join(pod_restart))
+    f.write("\n")
+    f.write("-"*40)
+    f.write("\n")
+    f.write(o)
+    f.write("\n")
+    f.write(e)
+    f.write("\n\n")
+
+
