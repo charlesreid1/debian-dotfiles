2765e6ff071591d792f7aa5d8b03772f69c51178
Charles Reid <charlesreid1@gmail.com>
2019-11-15 17:40:21 -0800

    add diff dotfiles script

--- dotfiles/bootstrap.sh
+++ dotfiles/bootstrap.sh
@@ -33,6 +33,7 @@ function doIt() {
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
     doIt;
 else
+    ./diff_dotfiles.sh
     read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
     echo "";
     if [[ $REPLY =~ ^[Yy]$ ]]; then
