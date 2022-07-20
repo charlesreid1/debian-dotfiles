af3d4c176823f4cbad449390b9db6ee24202c10c
Charles Reid <charlesreid1@gmail.com>
2022-07-19 22:50:50 -0700

    add script to generate ch4zm ssh key

---
+++ tasks/secrets/ch4zm_gen_ssh_keys.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# 
+# this is where you would add any
+# pre-baked public keys.
+set -x
+
+yes | ssh-keygen -f $HOME/.ssh/id_rsa_ch4zm -t rsa -N ''
+chmod 700 $HOME/.ssh
+touch $HOME/.ssh/authorized_keys
+chmod 600 $HOME/.ssh/authorized_keys
